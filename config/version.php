cafa24cabfa22d4525e22341ad0500f58eb4f142
zengin0 <150894855+zengin0@users.noreply.github.com>
2025-01-14 11:48:28 +0100

    1.1

--- config/version.php
+++ config/version.php
@@ -1,10 +1,13 @@
-<?php 
-
+<?php
+ 
     global $version;
-
-    $version['nummer'] = '1.0';
-
+ 
+    $version['nummer'] = '1.1';
+ 
     $versionen = array(
+        '1.1' => array(
+            '.gitignore angepasst'
+        ),
         '1.0' => array(
             'Erster Commit des Projekts'
         )
