9489c1a3f18fc1b95c0ac0f4940e1f04b7185496
Melina-0 <mh@inoya.de>
2025-01-15 11:23:16 +0100

    1.9

--- controllers/Form.php
+++ controllers/Form.php
@@ -1615,6 +1615,8 @@
 							
 							<?php if(isset($this->aktive_module['lackierer_kfz'])) $this->get_rechnung_angebot_position_fahrzeugdaten($praefix); ?>
 							<?php if(isset($this->aktive_module['teppichreinigung'])) $this->get_rechnung_angebot_position_teppichreinigung($praefix); ?>
+							<?php $this->get_rechnung_angebot_position_teppichreinigung($praefix); ?>
+							<?php $this->get_rechnung_angebot_position_fahrzeugdaten($praefix); ?>
 
 							<div class="row">
 								<div class="col-md-12">
